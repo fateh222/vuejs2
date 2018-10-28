738a77767230bc26e3cadd7bc1dbb3294f1eada5
Fateh Singh <fatehsingh.sunny5@gmail.com>
2018-10-28 20:35:46 +0530

    Tutorial -1 (Basic syntax of vuejs)

---
+++ l_1/app.js
@@ -0,0 +1,11 @@
+new Vue({
+  el: '#our-app',
+  data: {
+    message: 'This is our First Application',
+    infomation: {
+      first_name: 'Fateh',
+      last_name: 'Singh',
+      profession: 'Software Engineer'
+    }
+  }
+});
