daf59ff4ad2777ae16fa189b8d548630593419b2
Fateh Singh <fatehsingh.sunny5@gmail.com>
2018-10-28 20:39:57 +0530

    Tutorial -5 (Event Handling - Mouseover, Mouseout, Mousemove)

---
+++ l_5/app.js
@@ -0,0 +1,28 @@
+new Vue({
+  el: '#our-app',
+  data: {
+    message: 'Tutorial - 4 (Event Handling - Mouseover, Mouseout, Mousemove)',
+    infomation: {
+      first_name: 'Fateh',
+      last_name: 'Singh',
+      profession: 'Software Engineer'
+    },
+    x: 0,
+    y: 0,
+    txtOver: "Mouseover",
+    txtOut: "Mouseout"
+
+  },
+  methods: {
+    movefunc: function(event) {
+      this.x = event.offsetX;
+      this.y = event.offsetY;
+    },
+    overfunc: function() {
+      this.txtOver = " this is new text";
+    },
+    outfunc: function() {
+      this.txtOut = "after mouseout"
+    }
+  }
+});
