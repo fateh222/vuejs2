66a3a8deb7442e2c1dd09b1b7a793d6ea5d5d89f
Fateh Singh <fatehsingh.sunny5@gmail.com>
2018-10-29 21:29:38 +0530

    Tutorial - 4 (Event Handling - click and Double Click events)

--- l_4/app.js
+++ l_4/app.js
@@ -2,11 +2,6 @@ new Vue({
   el: '#our-app',
   data: {
     message: 'Tutorial - 4 (Event Handling - click and Double Click events)',
-    infomation: {
-      first_name: 'Fateh',
-      last_name: 'Singh',
-      profession: 'Software Engineer'
-    },
     counter: 0
   },
   methods: {
