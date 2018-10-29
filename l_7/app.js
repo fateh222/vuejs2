d7ff93be2cffe4b66471c2ccdeae3bc14afb25de
Fateh Singh <fatehsingh.sunny5@gmail.com>
2018-10-29 21:32:10 +0530

    Tutorial - 7 (Event Modifiers - once, prevent, stop, capture)

--- l_7/app.js
+++ l_7/app.js
@@ -2,11 +2,6 @@ new Vue({
   el: '#our-app',
   data: {
     message: 'Tutorial - 7 (Event Modifiers - once, prevent, stop, capture)',
-    infomation: {
-      first_name: 'Fateh',
-      last_name: 'Singh',
-      profession: 'Software Engineer'
-    },
     counter: 0
   },
   methods: {
