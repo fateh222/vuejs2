c38387ff19fc92239cbc24a890dfd3e44ce1f74a
Fateh Singh <fatehsingh.sunny5@gmail.com>
2018-10-29 21:27:38 +0530

    Tutorial -3 (Data Binding)

--- l_3/app.js
+++ l_3/app.js
@@ -2,11 +2,6 @@ new Vue({
   el: '#our-app',
   data: {
     message: 'Tutorial - 3 (Data Binding)',
-    infomation: {
-      first_name: 'Fateh',
-      last_name: 'Singh',
-      profession: 'Software Engineer'
-    },
     data_bind: {
       name: 'Khamb',
       url: 'https://www.youtube.com/watch?v=wjuIbiXtLqg',
